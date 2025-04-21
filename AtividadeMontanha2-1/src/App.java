b8c2ce761a9e2fe218b941852699d16498dae69e
Layanne Soares <125111381458@ulife.com.br>
2025-04-21 19:13:46 -0300

    Atividade 2 - Execicio 1

---
+++ AtividadeMontanha2-1/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int x = 1;
+        while (x<=50) {
+            System.out.println(x);
+            x++;
+            
+        }
+    }
+}
