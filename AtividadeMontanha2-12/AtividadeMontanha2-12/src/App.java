35760a88024700222694a8f031b8aa43556f72d1
Layanne Soares <layanneisabelle1305@gmail.com>
2025-04-24 19:33:25 -0300

    Atividade 2 - Exercicio 12

---
+++ AtividadeMontanha2-12/AtividadeMontanha2-12/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        for (int numeros = 1; numeros <= 100; numeros++) {
+            if (numeros % 2 != 0) {
+                System.out.println(numeros);
+            }
+        }
+    }
+}
